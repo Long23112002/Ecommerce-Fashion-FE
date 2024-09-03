0d5468cb0b47f3ac29e2406a662d2c5ec29dc950
Long23112002 <115551484+Long23112002@users.noreply.github.com>
2024-09-03 21:48:34 +0700

    fix

--- src/pages/Admin/User/AuthenticateGoogle.tsx
+++ src/pages/Admin/User/AuthenticateGoogle.tsx
@@ -1,14 +1,11 @@
 
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
-import {Box, CircularProgress, Typography} from "@mui/material";
 import {storeUserData} from "../../../api/AuthApi.ts";
 import { Box, Typography } from "@mui/material";
 import { Spin } from "antd";
 import Cookie from "js-cookie";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { storeUserData } from "../../../api/LoginApi.ts";
+
 
 export default function AuthenticateGoogle() {
     const navigate = useNavigate();
