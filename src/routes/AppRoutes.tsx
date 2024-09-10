7c8d67d9f7b546401740c0f66c598ca9ecaab856
mainghiep <nghiepmai2004@gmail.com>
2024-09-10 13:50:13 +0700

    ok

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import ProductManager from '../pages/Admin/Product';
 import AdminLayout from '../layouts/Admin';
 
@@ -6,7 +6,7 @@ import Login from '../pages/Admin/User/Login';
 import AuthenticateGoogle from '../pages/Admin/User/AuthenticateGoogle';
 import AuthenticateFacebook from '../pages/Admin/User/AuthenticateFacebook';
 import AdminRoute from "../hook/AdminRoute.tsx";
-import {ManagerRole, ManagerUser} from "../pages/Admin/User";
+import { ManagerRole, ManagerUser } from "../pages/Admin/User";
 import ForbiddenPage from "../pages/Error/ForbiddenPage.tsx";
 import UserLayout from "../layouts/Customer";
 import Home from "../pages/Customer/Home";
@@ -16,26 +16,26 @@ import ChatPage from "../pages/Admin/Chat";
 const AppRoutes = () => {
     return (
         <Routes>
-
-            <Route path="/authenticate" element={<AuthenticateGoogle/>}/>
-            <Route path="/authenticate-fb" element={<AuthenticateFacebook/>}/>
+            <Route path="/login" element={<Login />} />
+            <Route path="/authenticate" element={<AuthenticateGoogle />} />
+            <Route path="/authenticate-fb" element={<AuthenticateFacebook />} />
 
             <Route element={<UserLayout />}>
                 <Route path='/' element={<Home />} />
             </Route>
 
-            <Route element={<AdminRoute/>}>
-                <Route path="/login" element={<Login/>}/>
-                <Route element={<AdminLayout/>}>
-                    <Route path="/admin/product/*" element={<ProductManager/>}/>
-                    <Route path="/admin/user/role" element={<ManagerRole/>}/>
-                    <Route path="/admin/chat" element={<ChatPage/>}/>
-                    <Route path="/admin/user" element={<ManagerUser/>}/>
+            <Route element={<AdminRoute />}>
+
+                <Route element={<AdminLayout />}>
+                    <Route path="/admin/product/*" element={<ProductManager />} />
+                    <Route path="/admin/user/role" element={<ManagerRole />} />
+                    <Route path="/admin/chat" element={<ChatPage />} />
+                    <Route path="/admin/user" element={<ManagerUser />} />
                 </Route>
             </Route>
 
             {/* Route xử lý lỗi */}
-            <Route path="/403" element={<ForbiddenPage/>}/>
+            <Route path="/403" element={<ForbiddenPage />} />
         </Routes>
     );
 };
