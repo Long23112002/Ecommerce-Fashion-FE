874c1f39865fe1ad09b5a701e7dab530401cac43
mainghiep <nghiepmai2004@gmail.com>
2024-09-18 20:48:02 +0700

    olk

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -15,12 +15,9 @@ import ManagerSize from '../pages/Admin/Attributes/size/sizeIndex.tsx';
 import UserLayout from "../layouts/Customer";
 import Home from "../pages/Customer/Home";
 import ChatPage from "../pages/Admin/Chat";
-import ManagerBrand from '../pages/Admin/Product/brand/ManagerBrand.tsx';
 import ManaggerOrigin from '../pages/Admin/Product/origin/ManagerOrigin.tsx';
-
 import ManagerMaterial from '../pages/Admin/Attributes/material/materialIndex.tsx';
 import ManagerBrand from '../pages/Admin/Product/brand/ManagerBrand.tsx';
-import ManaggerOrigin from '../pages/Admin/Product/origin/ManagerOrigin.tsx';
 
 
 
@@ -28,33 +25,33 @@ import ManaggerOrigin from '../pages/Admin/Product/origin/ManagerOrigin.tsx';
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/authenticate" element={<AuthenticateGoogle/>}/>
-            <Route path="/authenticate-fb" element={<AuthenticateFacebook/>}/>
+            <Route path="/login" element={<Login />} />
+            <Route path="/authenticate" element={<AuthenticateGoogle />} />
+            <Route path="/authenticate-fb" element={<AuthenticateFacebook />} />
 
-            <Route element={<UserLayout/>}>
-                <Route path='/' element={<Home/>}/>
+            <Route element={<UserLayout />}>
+                <Route path='/' element={<Home />} />
             </Route>
 
 
-            <Route element={<AdminRoute/>}>
-                <Route path="/login" element={<Login/>}/>
-                <Route element={<AdminLayout/>}>
-                    <Route path="/admin/product/*" element={<ProductManager/>}/>
-                    <Route path="/admin/user/role" element={<ManagerRole/>}/>
-                    <Route path="/admin/brand" element={<ManagerBrand/>}/>
-                    <Route path="/admin/origin" element={<ManaggerOrigin/>}/>
-                    <Route path="/admin/color" element={<ManagerColor/>}/>
-                    <Route path="/admin/size" element={<ManagerSize/>}/>
-                    <Route path="/admin/material" element={<ManagerMaterial/>}/>
-                    <Route path="/admin/chat" element={<ChatPage/>}/>
-                    <Route path="/admin/user" element={<ManagerUser/>}/>
+            <Route element={<AdminRoute />}>
+                <Route path="/login" element={<Login />} />
+                <Route element={<AdminLayout />}>
+                    <Route path="/admin/product/*" element={<ProductManager />} />
+                    <Route path="/admin/user/role" element={<ManagerRole />} />
+                    <Route path="/admin/brand" element={<ManagerBrand />} />
+                    <Route path="/admin/origin" element={<ManaggerOrigin />} />
+                    <Route path="/admin/color" element={<ManagerColor />} />
+                    <Route path="/admin/size" element={<ManagerSize />} />
+                    <Route path="/admin/material" element={<ManagerMaterial />} />
+                    <Route path="/admin/chat" element={<ChatPage />} />
+                    <Route path="/admin/user" element={<ManagerUser />} />
                 </Route>
             </Route>
 
 
             {/* Route xử lý lỗi */}
-            <Route path="/403" element={<ForbiddenPage/>}/>
+            <Route path="/403" element={<ForbiddenPage />} />
         </Routes>
     );
 };
