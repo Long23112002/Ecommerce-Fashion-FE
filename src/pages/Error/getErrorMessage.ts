36d7912f80e4c1d1d00d57c3039149b76d1d6eac
namtp04 <namtran14022004@gmail.com>
2024-09-22 01:29:50 +0700

    fix validate

--- src/pages/Error/getErrorMessage.ts
+++ src/pages/Error/getErrorMessage.ts
@@ -1,19 +1,16 @@
 export const getErrorMessage = (error)=> {
-    // Kiểm tra nếu phản hồi từ API có chứa dữ liệu
+    
     if (error.response && error.response.data) {
       const { message } = error.response.data;
   
-      // Nếu message là một đối tượng và có trường "name", lấy giá trị của "name"
       if (typeof message === 'object' && message.name) {
         return message.name as string;
       }
   
-      // Nếu message là chuỗi, trả về chuỗi đó
       if (typeof message === 'string') {
         return message;
       }
     }
   
-    // Trả về thông báo mặc định nếu không có phản hồi API hoặc cấu trúc không mong đợi
     return 'Có lỗi xảy ra. Vui lòng thử lại sau.';
   };
