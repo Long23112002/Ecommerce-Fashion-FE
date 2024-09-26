99ff827d6e02fcf260dabf1952a22a7b6e1a2e5f
longnh <longjava2024@gmail.com>
2024-09-26 22:11:33 +0700

    fix conflic

--- src/components/Chat/ChatRoomBox.tsx
+++ src/components/Chat/ChatRoomBox.tsx
@@ -5,8 +5,8 @@ import { useSelector } from 'react-redux';
 import { callCreateChatRoom, callFindIdChatRoomByUserId } from '../../api/ChatApi';
 import { userSelector } from '../../redux/reducers/UserReducer';
 import ChatArea from './ChatArea';
-import LoginUserModel from '../User/LoginModelUser';
 import { UserOutlined } from '@ant-design/icons';
+import {LoginUserModel} from "../User/LoginModelUser.tsx";
 
 interface IProps {
   closeChat: () => void
