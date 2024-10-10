4554742978676dfc7d58d92d18d03f2c88da05f7
Trần Minh Thái <thaitranminh564@gmail.com>
2024-10-11 05:00:59 +0700

    update

--- src/components/Notification/NotificationItem.tsx
+++ src/components/Notification/NotificationItem.tsx
@@ -1,17 +1,34 @@
-import { Avatar, Box, Stack, Typography } from '@mui/material'
-import React from 'react'
+import { Avatar, Box, MenuItem, Stack, Typography } from '@mui/material'
+import { Parser } from 'html-to-react'
+import React, { useState } from 'react'
 import Notification from '../../types/Notification'
 import { formatDateTime } from '../../utils/timeUtils'
-import {Parser} from 'html-to-react'
+import MenuCustom from '../MenuCustom'
 
 interface IProps {
-    notification: Notification
+    notification: Notification,
+    handleMarkSeenByIdNoti: (id: string) => void
+    handleDeleteByIdNoti: (id: string) => void
 }
 
-const NotificationItem: React.FC<IProps> = ({ notification }) => {
+const NotificationItem: React.FC<IProps> = ({ notification, handleMarkSeenByIdNoti, handleDeleteByIdNoti }) => {
 
+    const [over, setOver] = useState<boolean>(false)
     const parser = Parser();
     const content = parser.parse(notification.content);
+
+    const handleOnMouseOver = () => {
+        setOver(true)
+    }
+
+    const handleOnMouseOut = () => {
+        setOver(false)
+    }
+
+    const handleMenu = (handleFunction: () => void) => {
+        handleOnMouseOut()
+        handleFunction()
+    }
 
     return (
         <Box
@@ -23,6 +40,8 @@ const NotificationItem: React.FC<IProps> = ({ notification }) => {
                 position: 'relative',
                 borderRadius: 2,
             }}
+            onMouseOver={handleOnMouseOver}
+            onMouseOut={handleOnMouseOut}
         >
             {
                 notification.avatar &&
@@ -58,6 +77,30 @@ const NotificationItem: React.FC<IProps> = ({ notification }) => {
                 >
                     {formatDateTime(notification.createAt)}
                 </Typography>
+
+                <MenuCustom
+                    open={over}
+                    sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        right: 25,
+                        transform: 'translateY(-50%)',
+                        display: over ? 'display' : 'none'
+                    }}
+                >
+                    <MenuItem
+                        onClick={() => handleMenu(() => handleMarkSeenByIdNoti(notification.id))}
+                    >
+                        <i className="fa-solid fa-eye me-3" />
+                        Đánh dấu đã xem
+                    </MenuItem>
+                    <MenuItem
+                        onClick={() => handleMenu(() => handleDeleteByIdNoti(notification.id))}
+                    >
+                        <i className="fa-solid fa-trash me-3" />
+                        Xóa thông báo
+                    </MenuItem>
+                </MenuCustom>
             </Stack>
 
             {!notification.seen && (
@@ -68,7 +111,6 @@ const NotificationItem: React.FC<IProps> = ({ notification }) => {
                         position: 'absolute',
                         top: '50%',
                         right: 0,
-                        transform: 'translateY(-50%)',
                         backgroundColor: '#00B8D9',
                         borderRadius: '50%',
                     }}
