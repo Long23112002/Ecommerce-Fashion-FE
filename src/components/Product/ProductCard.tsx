e4eb5b6d5aece6aa1bec87e930242fe01d3a4ddd
Trần Minh Thái <thaitranminh564@gmail.com>
2024-10-24 10:51:25 +0700

    fix scroll position

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardMedia, IconButton, Typography } from '@mui/material'
 import React from 'react'
 import Product from '../../types/Product'
-import { useNavigate } from 'react-router-dom'
+import useNavigate from '../../hook/useNavigateCustom';
 
 interface IProps {
     product: Product,
