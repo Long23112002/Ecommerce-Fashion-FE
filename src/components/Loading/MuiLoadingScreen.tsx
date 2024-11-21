494ffb977c1084e8c4d3b19553e77563243b261e
Trần Minh Thái <thaitranminh564@gmail.com>
2024-11-21 23:11:09 +0700

    update

--- src/components/Loading/MuiLoadingScreen.tsx
+++ src/components/Loading/MuiLoadingScreen.tsx
@@ -1,20 +1,12 @@
-import { Box } from '@mui/material';
-import { useSelector } from 'react-redux';
-import { loadingScreenSelector } from '../../redux/reducers/LoadingScreenReducer';
-import MuiLoading from './MuiLoading';
+import { Box } from '@mui/material'
+import MuiLoading from './MuiLoading'
 
 const MuiLoadingScreen = () => {
     return (
-        <Box
-            top={0}
-            left={0}
-            width='100%'
-            height='100vh'
-            bgcolor='rgba(0, 0, 0, 0.1)'
-        >
+        <Box position='fixed' top={0} left={0} bottom={0} right={0} bgcolor='rgb(0,0,0,0.2)' zIndex={99}>
             <MuiLoading size={55} />
         </Box>
-    );
-};
+    )
+}
 
-export default MuiLoadingScreen;
+export default MuiLoadingScreen
